d9cd0e727014b126c133529472b3479bdb18e354
chtello <chtello@escuelasproa.edu.ar>
2024-08-13 10:07:00 -0300

    imputs funcionando

---
+++ Practica/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+const [nombre,setNombre] = useState("");
+const [nro,setNro] = useState(0);
+const [contact,setContact] = useState([]);
+
+const newNombre = (event) =>{
+  setNombre(event.target.value)
+}
+const newNro = (event) =>{
+  setNro(event.target.value)
+} 
+const newContact = (event) => {
+  
+}
+  return (
+    <>
+      <h3>Agenda de contactos</h3>
+      <input onChange={newNombre} type="text" />
+      <input onChange={newNro} type="number" />
+      <button onClick={newContact}>Agregar Contacto</button>
+    </>
+  )
+}
+
+export default App
